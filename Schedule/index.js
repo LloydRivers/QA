a47ac16a549f94dd1815f04abda9aea64635a008
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-07-03 20:09:47 +0100

    added js to signup

--- Schedule/index.js
+++ Schedule/index.js
@@ -2,8 +2,6 @@ const listItems = document.querySelectorAll(".nav li a");
 
 listItems.forEach((item) => {
   item.addEventListener("click", (event) => {
-    event.preventDefault();
-
     listItems.forEach((item) => item.classList.remove("active"));
 
     item.classList.add("active");
